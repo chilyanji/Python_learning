63da843533f09ab4eb1cc80aecf7e90f46f8eaba
chilyanji <khankingc799@gmail.com>
2026-06-01 15:27:11 +0530

    Update new files

--- DSA/Day5/q2_binary_search_firstoccurance.py
+++ DSA/Day5/q2_binary_search_firstoccurance.py
@@ -1,20 +1,24 @@
 def searching(arr, target):
     left = 0
     right = len(arr) - 1
-    
+    result = -1  
     while left <= right:
         mid = (left + right) // 2
 
         if arr[mid] == target:
-            return mid
+            result = mid    
+            right = mid -1 
+            # left = mid + 1 
         elif target > arr[mid]:
             left = mid + 1
         else:
             right = mid - 1
-    return -1
+    return result
 
 
 
 arr = [1,2,2,2,3,4]
 target = 2
 print(searching(arr, target))
+
+
